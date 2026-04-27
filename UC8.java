fa9e69a2a3cdd6d1ed47db5db6a2c1ded78debf6
goonja26 <goonjata672@gmail.com>
2026-04-27 09:40:41 +0530

    Added UC8: Filter Passenger Bogies using streams

---
+++ UC8.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.util.stream.*;
+
+// Reusing Bogie class (same as UC7)
+class Bogie {
+    String name;
+    int capacity;
+
+    Bogie(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+    }
+
+    @Override
+    public String toString() {
+        return name + " -> " + capacity + " seats";
+    }
+}
+
+public class UC8 {
+
+    public static void main(String[] args) {
+
+        // Create list of bogies
+        List<Bogie> bogies = new ArrayList<>();
+        bogies.add(new Bogie("Sleeper", 72));
+        bogies.add(new Bogie("AC Chair", 78));
+        bogies.add(new Bogie("First Class", 24));
+
+        System.out.println("All Bogies:");
+        bogies.forEach(System.out::println);
+
+        // Stream filtering: capacity > 60
+        List<Bogie> filteredBogies = bogies.stream()
+                .filter(b -> b.capacity > 60)
+                .collect(Collectors.toList());
+
+        System.out.println("\nFiltered Bogies (Capacity > 60):");
+        filteredBogies.forEach(System.out::println);
+    }
+}
